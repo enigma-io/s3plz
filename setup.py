171cbadb17b20b4e194b9e46b59866cd0c3b2cab
abelsonlive <brianabelson@gmail.com>
2014-12-17 19:51:22 -0500

    cleanup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 def build():
     setup(
         name = "s3pyo",
-        version = "0.0.2",
+        version = "0.0.3",
         author = "Brian Abelson",
         author_email = "brian@enigma.io",
         description = "A polite interface for sending python objects to and from Amazon S3.",
