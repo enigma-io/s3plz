661037bd27e63c4b192bce8287c2d08ea8caa3e9
Brian Abelson <brianabelson@gmail.com>
2015-07-28 17:37:54 -0400

    Update __init__.py

--- s3plz/__init__.py
+++ s3plz/__init__.py
@@ -258,7 +258,7 @@ class S3:
             conn = boto.connect_s3(key, secret)
         except Exception as e:
             raise S3AuthError, \
-            "You're supplied credentials were invalid!"
+            "Your supplied credentials were invalid!"
 
         # lookup bucket
         for b in conn.get_all_buckets():
